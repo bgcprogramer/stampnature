e97ea545718c457162891c10d84a0201e15d10d1
Bao Gia Chau <chaugiabao96@gmail.com>
2020-04-26 20:28:20 -0400

    add logo

--- app.js
+++ app.js
@@ -14,16 +14,19 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost:27017/",{ useNewUrlParser: true, useUnifiedTopology: true  }, ()=>{
+mongoose.connect("mongodb://localhost:27017/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, () => {
   console.log("Database is connected successfully");
-})
+});
 
 
 app.route("/")
-  .get(function(req, res){
+  .get(function(req, res) {
     res.render("home")
   })
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log("Server is running on port 3000")
-})
+});
